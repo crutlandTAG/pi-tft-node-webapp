d4927f5522cd73acc3e93649181980e56ebf5ed0
Curtis Rutland <crutland@amherst.com>
2018-09-14 12:47:59 -0500

    added static files serving, spa routes, and index.html

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,41 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const path = require("path");
 const { screen, Colors } = require("./lib/screen");
 
 function setupMiddleware() {
   console.log("setting up expressjs middleware");
+  app.use(express.static(`${__dirname}/wwwroot`));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 }
 
 function setupRoutes() {
   console.log("setting up expressjs routes");
-  app.get("/", (req, res) => {
-    res.json({"message": "hello-world"});
+
+  app.get("/api/helloWorld", (req, res) => {
+    res.json({"message": "Hello World!"});
   });
 
-  app.post("/colors", (req, res) => {
+  app.post("api/color", (req, res) => {
     let color = req.body.color;
-    if(!color) color = "#FF0000";
+    if(!color) color = Colors.black;
     screen.setBackgroundColor(color);
     res.sendStatus(200);
   })
 
-  app.post("/", (req, res) => {
+  app.post("api/displayMessage", (req, res) => {
     let message = req.body.message;
     if(!message) message = "No Message Provided.";
-    screen.printText(`Message: ${message}`, Colors.black, Colors.red);
+    screen.printText(`Message: ${message}`, Colors.black, Colors.blue);
     setTimeout(() => screen.printText("Waiting..."), 3000);
     res.sendStatus(200);
-  });  
+  }); 
+
+  app.get("*", (req, res) => {
+    const index = path.resolve(__dirname, "index.html");
+    res.sendFile(index);
+  })
 }
 
 function setupServer() {
